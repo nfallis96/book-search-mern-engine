88c52b0e57ab59af59ddaa4fcfd1245fe55ef9e5
nfallis96 <nfallis2019@fau.edu>
2023-03-22 22:57:24 -0400

    edit resolvers js file on the server folder

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,72 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+const { User } = require("../models");
+
+// query function
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User
+                    .findOne({ _id: context.user._id })
+                    .select("-__v -password")
+                    .populate("books");
+                
+                return userData;
+            };
+            throw new AuthenticationError("log in required!");
+        },
+    }, 
+    Mutation: {
+        // to log in function
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError("Invalid login!");
+            };
+
+            const correctPW = await user.isCorrectPassword(password);
+            if (!correctPW) {
+                throw new AuthenticationError("Invalid login, try again!");
+            };
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        // saving books function
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updatedUser = await User
+                    .findOneAndUpdate(
+                        { _id: context.user._id }, 
+                        { $addToSet: { savedBooks: bookData } },
+                        { new: true },
+                    )
+                    .populate("books");
+                return updatedUser;
+            };
+            throw new AuthenticationError("logged in required to save books!");
+        },
+        // create users function      
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        // deleting books function
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true },
+                );
+                return updatedUser;
+            };
+            throw new AuthenticationError("logged in required to delete books!");
+        }
+    },
+};
+
+module.exports = resolvers;

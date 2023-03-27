4d4e3bf5a235e95da14a1029dbf4cb05e389006d
nfallis96 <nfallis2019@fau.edu>
2023-03-27 16:30:14 -0400

    fix bugs

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,19 +6,21 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
+  // removed res & next from parameters
   authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.body.token || let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
-    };
+    }
 
-
+    // removed res.status... replaced with req
     if (!token) {
-      return req;
-    };
+      return req
+      // res.status(400).json({ message: 'You have no token!' });
+    }
 
     // verify token and get user data out of it
     try {
@@ -26,11 +28,13 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-    };
+      // return res.status(400).json({ message: 'invalid token!' });
+    }
+
+    return req;
 
     // send to next endpoint
-    return req;
-   
+    // next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
